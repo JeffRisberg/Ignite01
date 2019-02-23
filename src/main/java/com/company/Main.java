8245b923e7c6909ec6c0850407dfc24ca86c93e5
JeffRisberg <jrisberg@aol.com>
2019-02-23 10:29:03 -0800

    set up configuration

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,18 +1,24 @@
 package com.company;
 
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.Ignition;
+
 public class Main {
 
   public static void main(String[] args) {
 
-    try (Ignite ignite = Ignition.start("examples/config/example-ignite.xml")) {
+    try (Ignite ignite = Ignition.start("example-ignite.xml")) {
       IgniteCache<Integer, String> cache = ignite.getOrCreateCache("myCacheName");
 
       // Store keys in cache (values will end up on different cache nodes).
-      for (int i = 0; i < 10; i++)
+      for (int i = 0; i < 10; i++) {
         cache.put(i, Integer.toString(i));
+      }
 
-      for (int i = 0; i < 10; i++)
+      for (int i = 0; i < 10; i++) {
         System.out.println("Got [key=" + i + ", val=" + cache.get(i) + ']');
+      }
     }
   }
 }
